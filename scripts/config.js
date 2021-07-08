78b56dac464467705ca6e8b9e988e14c0a0126f0
roojay <roojay520@gmail.com>
2021-07-08 16:21:53 +0800

    chore: 初始化项目

---
+++ scripts/config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: roojay
+ * @Description: 打包相关的配置文件
+ */
+
+const config = {
+  pkgName: 'bobplug-nbnhhsh',
+  github:{
+    username: 'roojay520',
+    repository: 'bobplug-nbnhhsh'
+  }
+};
+
+module.exports = config;
